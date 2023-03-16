8a9b38b04b0c14a17f5f578ba8cd283326ccd253
jclync <jocelynchen1126@yahoo.com>
2023-03-16 14:51:07 -0700

    add music and sprite animation

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+let char1, char2;
+let charSwitched = false;
+let song;
+
 let cookieChar, ground, blobs;
 let spriteX, spriteY, spriteSize, spriteSpeed;
 
@@ -10,28 +14,60 @@ let threeLives = "Lives: ❤️‍🔥 ❤️‍🔥 ❤️‍🔥";
 let messageSent = false;
 
 function preload() {
-    happyBee = loadImage('images/happyBobaBee.gif');
-    sadBee= loadImage('images/sadBobaBee.gif');
-    cheerBee = loadImage('images/cheerBobaBee.gif')
     owSong = loadSound('audio/ow.mp3');
     wrongSong = loadSound('audio/wrong.mp3');
+    song = loadSound('audio/music.mp3');
+
+  	char1 = new Sprite(80, 200, 74, 60);
+	char1.spriteSheet = 'images/spriteSheet1.png';
+    char1.anis.offset.y = 2;
+	char1.anis.frameDelay = 8;
+
+	char1.addAnis({
+		run: { row: 0, frames: 5},
+        twoLives: {row: 1, frames: 5},
+        oneLife: {row: 2, frames: 5},
+        dead: {row: 3, frames: 5}
+	});
+	char1.ani = 'run';
+  
+
+    char2 = new Sprite(80, 200, 74, 60);
+	char2.spriteSheet = 'images/spriteSheet2.png';
+	char2.anis.frameDelay = 8;
+
+	char2.addAnis({
+		run: { row: 0, frames: 5},
+        twoLives: {row: 1, frames: 5},
+        oneLife: {row: 2, frames: 5},
+        dead: {row: 3, frames: 5}
+	});
+    
+	char1.ani = 'run';
+    char2.ani = 'run';
+    char1.overlaps(char2);
 }
 
 function setup() {
     createCanvas(800, 450);
-
-    // resize character sizes
-    //happyBee.resize(70, 70);
-    cheerBee.resize(85,85);
-    sadBee.resize(60, 60); 
+    textFont('Courier New');
+    song.play();
+    song.setVolume(0.6);
+    owSong.setVolume(1);
+    allSprites.pixelPerfect = true; // the sprite will be drawn at integer coordinates
+    //createCanvas(125, 48, 'pixelated x4');
 
     spriteX = 80;
     spriteSize = 40;
     spriteY = height/2 - spriteSize/2;
-    ground = new Sprite(0, height/4*3, width*3, 5, 'static');
+    ground = new Sprite(0, height/4*3, width*3, 10, 'static');
+    ground.color = 'green';
     blobs = new Group();
-    cookieChar = new Sprite(spriteX, spriteY, spriteSize);
-    cookieChar.img = cheerBee;
+    cookieChar = new Sprite(spriteX, 315, 40, 40);
+    char1.overlaps(cookieChar);
+    char2.overlaps(cookieChar);
+    cookieChar.visible = false;
+    char2.visible = false;
     world.gravity.y = 10.8; 
     
     // set scores and stuff
@@ -45,7 +81,6 @@ function setup() {
 function draw() {
     if (!gameOver) {
         playGame();
-        //cookieChar.debug = mouse.pressing();
     }
 }
 
@@ -53,17 +88,23 @@ function playGame() {
     //set game screen text
     setGameScreen();
 
+    // randomize appearance of obstacles 
     if (random(1) < 0.01) {
-        new blobs.Sprite(width, (height/4*3) - spriteSize/2, spriteSize);
+        new blobs.Sprite(width, (height/4*3) - spriteSize/2, spriteSize/2);
     }
 
     spriteSpeed = 2 + sqrt(currentScore)/5;
     blobs.move(width*2, 'left', spriteSpeed);
 
-    if (cookieChar.collides(blobs)) {
-        blobs.removeAll();
-        cookieChar.sleeping = true; 
-        updateLives();
+    if (blobs.overlapping(char1) || blobs.overlapping(char2)) {
+        if (cookieChar.collides(blobs)) {
+            blobs.removeAll();
+            char1.position = {x: 80, y: 300};
+            cookieChar.position = {x: 80, y: 315};
+            char2.position = {x: 80, y: 300};
+          
+            updateLives();
+        }
     }
 }
 
@@ -77,36 +118,50 @@ function setGameScreen() {
     text(life + '\nScore: ' + round(currentScore), 30, 30);
     textAlign(RIGHT);
     text('High Score: ' + highScore, width - 30, 30); 
-    text('Press space or up arrow to jump. \n Press (q) to quit.', width - 30, (height / 4 * 3) + 50);
+    text('Press space or up arrow to jump. \n Press (c) to change character. \n Press (q) to quit.', width - 30, (height / 4 * 3) + 50);
 }
 
 // 'space' or uparrow to jump; enter or 'r' to restart
 function keyPressed() {
     if (!gameOver) {
-        // up arrow
-        if (keyCode == UP_ARROW) {
+        if (keyCode == UP_ARROW) { // up arrow
+            char1.move(125, 'up', 6.5);
+            char2.move(125, 'up', 6.5);
             cookieChar.move(125, 'up', 6.5);
-        // space
-        } else if (key == ' ') { 
+        } else if (key == ' ') { // space
+            char1.move(125, 'up', 6.5);
+            char2.move(125, 'up', 6.5);
             cookieChar.move(125, 'up', 6.5);
         } 
     } else {
-        // enter key
-        if (keyCode == '13') {
+        if (keyCode == '13') { // enter
             restart();
             playGame();
         }
     }
     
-    // 'r' key
-    if (keyCode == '82') {
+    if (keyCode == '82') { // 'r' key
         restart();
         playGame();
-    // 'q' key
-    } else if (keyCode == '81') {
+    } else if (keyCode == '81') { // 'q' key
         endGame();
+    } else if (keyCode == '67') { // 'c' key
+        switchCharacter();
     }
     return false;
+}
+
+function switchCharacter() {
+    //count++;
+  if (!charSwitched) {
+      char1.visible = true;
+      char2.visible = false;
+      charSwitched = true;
+  } else {
+      char1.visible = false;
+      char2.visible = true;
+      charSwitched = false;
+  }
 }
 
 function checkLives() {
@@ -114,8 +169,12 @@ function checkLives() {
         life = threeLives;
     } else if (lives == 2) {
         life = twoLives;
+        char1.ani = 'twoLives';
+        char2.ani = 'twoLives';
     } else if (lives == 1) {
         life = oneLife;
+        char1.ani = 'oneLife';
+        char2.ani = 'oneLife';
     } else {
         life = noLives;
     }
@@ -124,20 +183,19 @@ function checkLives() {
 function updateLives() {
     owSong.play();
     lives--;
-    console.log(lives);
     
     if (lives == 0) {
-        console.log("GAME OVER");
         endGame();
     }
 }
 
 function endGame() {
     gameOver = true;
-    
-    // reset and clear screen
-    cookieChar.sleeping = true;
-    cookieChar.img = sadBee;
+
+    char1.sleeping = true;
+    char1.ani = 'dead';
+    char2.sleeping = true;
+    char2.ani = 'dead';
 
     blobs.removeAll();
     background(0);
@@ -147,9 +205,11 @@ function endGame() {
     textAlign(CENTER);
     textSize(40);
     fill('red');
+    textStyle(BOLD);
     text("Game Over 😔", width / 2, height / 5);
     textSize(20);
     fill(255);
+    textStyle(NORMAL);
     text("Your Score: " + round(currentScore) + "\n Your High Score: " + round(highScore) + "\n \n \n Press enter to play again!", width / 2, height / 3);
     
     /* FIREBASE
@@ -172,9 +232,9 @@ function restart() {
     gameOver = false;
     messageSent = false;
 
-    //reset bee
-    cookieChar.img = cheerBee;
-    cookieChar.rotation = 0;
-    cookieChar.position = {x: 80, y: 205};
+    //reset character
+    cookieChar.position = {x: 80, y: 315};
     world.gravity.y = 10.8; 
+    char1.ani = 'run';
+    char2.ani = 'run';
 }
